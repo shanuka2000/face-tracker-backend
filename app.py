b1ab017e67591451aff0ae46d64107a491962d19
shanuka2000 <shanuka.inbox@gmail.com>
2023-07-05 16:19:30 +0530

    Added mongoDB connection data

--- app.py
+++ app.py
@@ -7,6 +7,19 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+# DB Configs
+usename = "shanukase"
+password = "8Ejay0sufP6hTbOM"
+cluster_name = "cluster0"
+database_name = "face-tracker"
+collection_name = "sessionData"
+
+# Create a MongoDB client
+connection_string = f"mongodb+srv://{usename}:{password}@{cluster_name}.yj3jrtg.mongodb.net/{database_name}?retryWrites=true&w=majority"
+client = MongoClient(connection_string)
+db = client[database_name]
+collection = db[collection_name]
+
 # Health Check endpoint
 @app.route('/', methods=["GET"])
 def check_health():
