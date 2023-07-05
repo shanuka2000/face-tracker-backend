cfe10729d151fbcd57a3a37c8e020966d157d963
shanuka2000 <shanuka.inbox@gmail.com>
2023-07-05 14:45:52 +0530

    Initial code commit for face-tracker backend

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
