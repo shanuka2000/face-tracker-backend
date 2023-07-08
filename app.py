57aa2f48a084824861b8c5b259f44e95e46c4d40
shanuka2000 <shanuka.inbox@gmail.com>
2023-07-09 01:40:21 +0530

    Implemented generate image function

--- app.py
+++ app.py
@@ -2,10 +2,17 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from flask_cors import CORS
+from keras.models import load_model
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+import os
+import time
 
 # app configs
 app = Flask(__name__)
 CORS(app)
+app.static_folder = 'static'
 
 # DB Configs
 usename = "shanukase"
@@ -20,7 +27,61 @@ client = MongoClient(connection_string)
 db = client[database_name]
 collection = db[collection_name]
 
+
+def generate_images(num_samples, sampled_labels):
+    latent_dim = 100
+
+    # Load the generator model
+    generator = load_model('generator_model.h5')
+
+    # Generate noise vectors
+    noise = np.random.normal(0, 1, (num_samples, latent_dim))
+
+    # Use the generator to create new images
+    generated_images = generator.predict([noise, sampled_labels])
+
+    # Rescale images from range -1 to 1 to range 0 to 1
+    generated_images = 0.5 * generated_images + 0.5
+
+    # Save the generated images as files and get their paths
+    image_paths = []
+    timestamp = int(time.time())  # Get a timestamp for uniqueness
+    for i, img in enumerate(generated_images):
+        # Create a unique filename for each image
+        filename = f"image_{timestamp}_{i+1}.png"
+
+        # Save the image as a file
+        save_path = os.path.join("static/images_generated", filename)
+        plt.imsave(save_path, img)
+
+        # Append the image path to the list
+        image_paths.append(save_path)
+
+    return image_paths
+
+# Generate image endpoint
+
+
+@app.route('/features/generate', methods=["POST"])
+def generate_image():
+    try:
+        # return json_data
+        sampled_labels = np.array([list(request.get_json())])
+        generated_image_paths = generate_images(
+            num_samples=1, sampled_labels=sampled_labels)
+
+        # Return the file paths as a JSON response
+        response_data = {
+            "image_paths": generated_image_paths
+        }
+
+        return json.dumps(response_data)
+    except Exception as e:
+        return f"Error generating image: {str(e)}"
+
 # Save session data endpoint
+
+
 @app.route('/session/save', methods=["POST"])
 def save_session():
     try:
@@ -31,6 +92,8 @@ def save_session():
         return f"Error saving data: {str(e)}"
 
 # Get one item
+
+
 @app.route("/session/one", methods=["GET"])
 def get_one_session():
     try:
@@ -44,6 +107,8 @@ def get_one_session():
         return f"Error retrieving data: {str(e)}"
 
 # Get all data
+
+
 @app.route('/session/all', methods=["GET"])
 def get_sessions():
     try:
@@ -59,6 +124,8 @@ def get_sessions():
         return f"Error retrieving data: {str(e)}"
 
 # Health Check endpoint
+
+
 @app.route('/', methods=["GET"])
 def check_health():
     try:
@@ -67,6 +134,6 @@ def check_health():
     except Exception as e:
         return f"Connection failed to MongoDB Atlas", 500
 
-# 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
